f8b1ec4a4912da0bcc05dd786f0eba3243168a63
eselbeus <eselbeus@gmail.com>
2019-01-11 10:21:51 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,12 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
-
+  items = groceries.values
+  final_list = []
+  items.each do |food_group|
+    food_group.each do |food|
+      final_list << food
+    end
+  end
+  return final_list
 end
